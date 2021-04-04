65fd10a1041b6df30d7beb2b19cea552d0a30f5c
Mihir Kapur <mihirkapur96@gmail.com>
2021-04-04 14:54:41 -0700

    Changes

--- js/ch23ex3.js
+++ js/ch23ex3.js
@@ -1,1 +1,16 @@
 console.log("Mihir's output from Chapter 23 Exercise 3");
+document.querySelector("form").addEventListener("submit", e => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    fetch("http://127.0.0.1:5500/html/ch23ex3.html", {
+      method: "POST",
+      body: formData
+    })
+      .then(response => response.text())
+      .then(result => {
+        document.getElementById("result").textContent = result;
+      })
+      .catch(err => {
+        console.error(err.message);
+      });
+  });
